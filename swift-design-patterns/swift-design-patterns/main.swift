1f830f872072cd39c22540c24d1a44c46b6ac5a7
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2017-10-26 23:51:17 -0400

    Initial Empty Project

---
+++ swift-design-patterns/swift-design-patterns/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  swift-design-patterns
+//
+//  Created by Aaron Hinton on 10/26/17.
+//  Copyright © 2017 No Name Software. All rights reserved.
+//
+
+import Foundation
+
+print("Yay! Design Patterns!")
